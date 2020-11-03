308928ddd577059842ff213072a0692c5eaafc0e
bshk102 <xartem.kx@gmail.com>
2020-11-03 21:32:25 +0500

    App refactor to ClassComponent, add function 'delete todo item'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './app.css'
 import AppHeader from '../app-header'
 import SearchPanel from '../search-panel'
 import TodoList from '../todo-list'
 import ItemStatusFilter from '../item-status-filter'
 
-const App = () => {
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            todoData : [
+                { label: 'Drink Cofee', id: 1 },
+                { label: 'Build React App', id: 2 },
+                { label: 'Have a lunch', id: 3 }
+            ]
+        }
 
-    const todoData = [
-        { label: 'Drink Cofee', id: 1 },
-        { label: 'Build React App', id: 2 },
-        { label: 'Have a lunch', id: 3 }
-    ]
+        this.deleteItem = (id) => {
+            this.setState(({ todoData }) => {
+                const idx = todoData.findIndex(el => el.id === id)
+                return {
+                    todoData: [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]
+                }
+            })
+        }
+    }
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                
+                <TodoList 
+                    todoData={ this.state.todoData }
+                    onDeleted={ this.deleteItem } />
             </div>
-            
-            <TodoList todoData={todoData} />
-        </div>
-    )
+        )       
+    }
 }
-
-export default App
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -25,7 +25,7 @@ export default class TodoListItem extends Component {
     }
 
     render() {
-        const { label } = this.props
+        const { label, onDeleted } = this.props
         const { done, important } = this.state
 
         let classNames = 'todo-list-item'
@@ -43,7 +43,7 @@ export default class TodoListItem extends Component {
                 <span
                     className="todo-list-item-label"
                     onClick={ this.onLabelClick }
-                    important={ this.state.important }
+                    important={ important }
                     >
                     { label }
                 </span>
@@ -55,7 +55,8 @@ export default class TodoListItem extends Component {
                 </button>
     
                 <button type="button"
-                        className="btn btn-outline-danger btn-sm float-right">
+                        className="btn btn-outline-danger btn-sm float-right"
+                        onClick={ onDeleted }>
                     <i className="fa fa-trash-o" />
                 </button>
             </span>
